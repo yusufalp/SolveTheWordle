fc8c5361c4f81a9895dc7e30cb1486ce1a1a729a
Yusuf Alp <alpyusuf007@gmail.com>
2022-03-19 18:56:38 -0700

    render components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Form from './Form';
+import Words from './Words';
+import Result from './Result';
+import { wordsList } from '../wordsList';
+
 import '../css/App.css';
 
 function App() {
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [userGuesses, setUserGuesses] = useState([]);
+  const [userGuessAnalysis, setUserGuessAnalysis] = useState([]);
+  const [possibleAnswers, setPossibleAnswers] = useState(wordsList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Solve The Wordle</h1>
-      </header>
+      <Header />
+      {userGuessAnalysis.map((analysis, ind) => (
+        <Words
+          key={ind}
+          indX={ind}
+          userGuessAnalysis={userGuessAnalysis}
+          setUserGuessAnalysis={setUserGuessAnalysis}
+          analysis={analysis}
+        />
+      ))}
+      <Form
+        currentGuess={currentGuess}
+        setCurrentGuess={setCurrentGuess}
+        userGuesses={userGuesses}
+        setUserGuesses={setUserGuesses}
+        userGuessAnalysis={userGuessAnalysis}
+        setUserGuessAnalysis={setUserGuessAnalysis}
+      />
+      {userGuesses.length >= 1 &&
+        <Result
+          possibleAnswers={possibleAnswers}
+          userGuessAnalysis={userGuessAnalysis}
+        />}
     </div>
   );
 }
