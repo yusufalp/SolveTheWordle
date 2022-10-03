24831d43a4306c79754684746565a6af97d69304
Yusuf Alp <alpyusuf007@gmail.com>
2022-10-02 20:41:16 -0700

    import React

--- src/components/shared/FooterLogo/index.js
+++ src/components/shared/FooterLogo/index.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const FooterLogo = ({ classes, logo, link }) => {
   return (
     <a href={link} className={classes} target="_blank" rel="noreferrer">
       <i className={`fa-brands fa-${logo}`}></i>
     </a>
-  )
+  );
 }
 
 export default FooterLogo;
