35ace8a509abdd6d8ddb8ab89f39eba80f00d442
Yusuf Alp <alpyusuf007@gmail.com>
2022-10-08 22:05:30 -0700

    add testid to the element

--- src/components/shared/FooterLogo/index.js
+++ src/components/shared/FooterLogo/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './style.css';
+
 const FooterLogo = ({ classes, logo, link }) => {
   return (
     <a href={link} className={classes} target="_blank" rel="noreferrer">
-      <i className={`fa-brands fa-${logo}`}></i>
+      <i className={`fa-brands fa-${logo}`} data-testid="icon-element"></i>
     </a>
   );
 }
