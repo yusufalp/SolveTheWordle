f7797b482812195539357a7080234969a5d517ca
Yusuf Alp <alpyusuf007@gmail.com>
2022-10-09 23:19:41 -0700

    update state properly

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
+import './style.css';
 
 const Grid = ({ indX, indY, userGuessAnalysis, setUserGuessAnalysis, letter }) => {
   const [color, setColor] = useState("bg-black");
 
   const handleClick = () => {
+    const updatedUserGuessAnalysis = [...userGuessAnalysis];
+
     if (color === "bg-black") {
-      userGuessAnalysis[indX][indY].includes = true;
+      updatedUserGuessAnalysis[indX][indY].includes = true;
       setColor("bg-yellow");
     } else if (color === "bg-yellow") {
-      userGuessAnalysis[indX][indY].position = true;
+      updatedUserGuessAnalysis[indX][indY].position = true;
       setColor("bg-green");
     } else {
-      userGuessAnalysis[indX][indY].position = false;
-      userGuessAnalysis[indX][indY].includes = false;
+      updatedUserGuessAnalysis[indX][indY].position = false;
+      updatedUserGuessAnalysis[indX][indY].includes = false;
       setColor("bg-black");
     }
-    let newUserGuessAnalysis = [...userGuessAnalysis];
-    setUserGuessAnalysis(newUserGuessAnalysis);
+
+    setUserGuessAnalysis(updatedUserGuessAnalysis);
   }
 
   return (
