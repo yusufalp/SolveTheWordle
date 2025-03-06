3fc9e21650954a1b473340d5ce6442e6090980c0
yusufalp <alpyusuf007@gmail.com>
2025-03-05 22:43:58 -0800

    refactor stylesheet

--- src/components/shared/FooterLogo/index.js
+++ src/components/shared/FooterLogo/index.js
@@ -4,7 +4,12 @@ import "./style.css";
 
 const FooterLogo = ({ className, logo, link }) => {
   return (
-    <a href={link} className={className} target="_blank" rel="noreferrer">
+    <a
+      href={link}
+      className={`${className} logo`}
+      target="_blank"
+      rel="noreferrer"
+    >
       <i className={`fa-brands fa-${logo}`} data-testid="icon-element"></i>
     </a>
   );
